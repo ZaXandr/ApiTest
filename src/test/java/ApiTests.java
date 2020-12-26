2d5d392c2ec8f2fb37d3a206db88b2d0522b6edb
ZaXandr <75754141+ZaXandr@users.noreply.github.com>
2020-12-26 13:53:24 +0200

    Api

---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,72 @@
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.EncoderConfig.encoderConfig;
+
+public class ApiTests {
+
+//    Cover the next endpoints with tests and include into in CI pipeline:
+//    - Upload
+//    - GetFileMetadata
+//    - Delete file
+
+    @Test
+    public void uploadFileTest() {
+        // rest assured adds charset specification by default which is not expected by dropbox and results in an error
+        RestAssured.config = RestAssured.config().encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
+
+        File file = new File("src/test/resources/Smile.jpg");
+        String apiArgs = "{\"mode\":\"add\",\"autorename\":true,\"mute\":false,\"path\":\"/Smile.jpg\"}";
+
+        given()
+                .headers("Authorization","Bearer CPPB8UPk0osAAAAAAAAYL3jj0is7XQDTDIpvzO7jl0J0BYqr3bPlemDhBBl1WbkH",
+                        "Content-Type", "application/octet-stream",
+                        "Dropbox-API-Arg", apiArgs)
+                .body(file)
+                .when()
+                .post("https://content.dropboxapi.com/2/files/upload")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getFileMetadataTest() {
+        Map<String,String> path = new HashMap<>();
+        path.put("path", "/Smile.jpg");
+
+        given()
+                .headers("Authorization","Bearer CPPB8UPk0osAAAAAAAAYL3jj0is7XQDTDIpvzO7jl0J0BYqr3bPlemDhBBl1WbkH",
+                        "Content-Type", "application/json")
+                .body(path)
+                .when()
+                .post("https://api.dropboxapi.com/2/files/get_metadata")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void deleteFileTest() {
+        Map<String,String> path = new HashMap<>();
+        path.put("path", "/Smile.jpg");
+
+        given()
+                .headers("Authorization","Bearer CPPB8UPk0osAAAAAAAAYL3jj0is7XQDTDIpvzO7jl0J0BYqr3bPlemDhBBl1WbkH",
+                        "Content-Type", "application/json")
+                .body(path)
+                .when()
+                .post("https://api.dropboxapi.com/2/files/delete_v2")
+                .then()
+                .statusCode(200);
+    }
+
+    public static void main() {
+
+    }
+}
